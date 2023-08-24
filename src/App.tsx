d495519b05a8ba730172c8f426a946f5ad6ce8a3
Maksim-Jk <sinplym1@gmail.com>
2023-08-24 18:12:12 +0500

    --ammend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react'
 import './App.css'
 import Router from './router/Router'
-import Main from './pages/Main'
 
 const App: FC = () => {
 
@@ -9,5 +8,6 @@ const App: FC = () => {
     <Router/>
   )
 }
+
 
 export default App
