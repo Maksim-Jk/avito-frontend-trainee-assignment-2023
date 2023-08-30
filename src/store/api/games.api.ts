508c66f33c9318fcf67e8461120e339f2fde04d7
Maksim-Jk <sinplym1@gmail.com>
2023-08-30 15:18:09 +0500

    feat: change api url and add headers, fix versel

--- src/store/api/games.api.ts
+++ src/store/api/games.api.ts
@@ -2,13 +2,17 @@ import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import { IGame, IGameById, IGameQuery } from "../../types/games.types";
 import { transformData } from "../../utils/transformData";
 
-const API_URL = "https://www.freetogame.com/api/";
-
+const API_URL = "https://free-to-play-games-database.p.rapidapi.com/api/";
+const API_HEADERS = {
+  'X-RapidAPI-Key': 'a27719d883msh123a8e1f7666d4cp119590jsn30415a721b08',
+  'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+}
 
 export const gamesApi = createApi({
   reducerPath: "gamesApi",
   baseQuery: retry(fetchBaseQuery({
     baseUrl: API_URL,
+    headers: API_HEADERS
   }), {maxRetries : 3}
   ),
   endpoints: (builder) => ({
